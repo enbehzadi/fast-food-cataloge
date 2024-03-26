60ee80e9aeae912b179845bc448aaee4f99a48bf
m.behzadi <enbehzadi@gmail.com>
2024-03-26 16:38:14 +0330

    add list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import CategoryList from './CategoryList/CategoryList';
 import Header from './Header/header';
+import axios from './axios';
+import Loading from './Loading/loading';
 
+import FastFoodList from './FastFoodList/fastFoodLIst';
 function App() {
+  const [loading,setLoading]=useState(false);
+ 
+  const [fastFoodItems,setFastFoods]=useState([]);
+  const fetchData = async(categoryId=null) => {
+    setLoading(true);
+    const responce=await axios.get(`/FastFood/list/${categoryId ? "?categoryId=" + categoryId : ""}`);
+    console.log(responce.data);
+    setFastFoods(responce.data);
+
+  };
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const renderContent=()=>{
+    // if(Loading){
+    //   return  <loading theme="dark"/>
+    // }
+    return <FastFoodList fastFoodItems={fastFoodItems}/>
+  }
   return (
       <div className='wrapper bg-faded-dark'>
       <Header>
       </Header>
       <CategoryList></CategoryList>
+      
+      <div className='container mt-4'>
+      {
+        renderContent()
+      }
+      </div>
       </div>
   );
 }
--- src/CategoryList/CategoryList.jsx
+++ src/CategoryList/CategoryList.jsx
@@ -1,27 +1,28 @@
 import { useEffect } from "react";
 import axios from "../axios";
 import { useState } from "react";
-import Loading  from "../Loading/Loading";
+import Loading  from "../Loading/loading";
 
 const CategoryList=()=>{
-    const [loading,setLoading]=useState(true);
+    // const [loading,setLoading]=useState(true);
     const [categories,setCategories]=useState([]);
   useEffect(()=>{
         const fetchCategories=async()=>{
             const responce=await axios.get('/FoodCategory/categories');
 
             setCategories(responce.data);
-            setLoading(false);
-        }
+            // setLoading(false);
+        } 
+        
         fetchCategories();
   },[] 
   );
   const renderContent=()=>{
-    if(loading){
-        return <loading/>
-    }
+    // if(loading){
+    //     return <loading/>
+    // }
     return(
-        <ul className="nav">
+        <ul className="nav d-flex justify-content-end">
         <li className="nav-item" >
             <a className="nav-link" href="#">
                 همه فست فود ها
@@ -40,13 +41,14 @@ const CategoryList=()=>{
   }
   return(
     <nav className="container mt-n5">
-            <div className="bg-white rounded-3 shadow-lg py-4" style={{heigh:'80px'}}>
-              
-
-            </div>
+            <div
+        className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4 " 
+        style={{ height: "40px", justifyContent:"right"}}
+      >   
             {
                 renderContent()
             }
+              </div>
     </nav>
 );
 }
